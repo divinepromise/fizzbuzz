878e6ef09ff05a5159747d0eac3a68842e524282
divinepromise <divinepromise@dal.ca>
2019-05-23 07:15:07 -0700

    added a try and except block to handle error from the user

--- task1.py
+++ task1.py
@@ -1,27 +1,30 @@
-def l33t(book_name):
-	file = open(book_name)
+def l33t():
+
+	try:
+		userinput = input("enter the exact bookname with its extension\n")
+
+		file = open(userinput)
 	
-	for line in file:
-		word = line.strip()
-		print(word)
-
-		word = "aheiiager"
-
-		if word.endswith("er"):
-			strip = word[:-2]
-			new_word = strip + "xor"
+		for line in file:
+			word = line.strip()
+			
+			if word.endswith("er"):
+				strip = word[:-2]
+				new_word = strip + "xor"
 	
-		for char in new_word:
-			if char == "o" or "O":
-				char = 0
-			elif char == "a" or "A":
-				char = 4
-			elif char == "e" or "E":
-				char = 3
-			elif char == "i" or "I":
-				char = 1
+			for char in new_word:
+				if char == "o" or "O":
+					char = 0
+				elif char == "a" or "A":
+					char = 4
+				elif char == "e" or "E":
+					char = 3
+				elif char == "i" or "I":
+					char = 1
+			print(new_word)
+	except:
+		print("please enter a string value")
 
 
-		print(new_word)
 
 l33t("Book1.txt")
